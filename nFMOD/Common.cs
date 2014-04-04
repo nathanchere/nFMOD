8055e6489b0a51ddc25ea9531c4694749a6c15a0
Nathan Chere <git@nathanchere.com.au>
2014-04-04 15:45:46 +1100

    Common configuration

---
+++ nFMOD/Common.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nFMOD
+{
+    public static class Common
+    {
+        #if (_WIN64) 
+        public const string FMOD_DLL = "fmodex64";
+        #else
+        public const string FMOD_DLL = "fmodex";
+        #endif
+    }
+}
