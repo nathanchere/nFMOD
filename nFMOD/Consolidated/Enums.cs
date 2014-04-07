897192260aa267c1ce5b0b1ddd3675c7b377e42c
Nathan Chere <git@nathanchere.com.au>
2014-04-07 17:18:45 +1000

    AdvancedSettings

--- nFMOD/Consolidated/Enums.cs
+++ nFMOD/Consolidated/Enums.cs
@@ -2,7 +2,213 @@
 using System.Runtime.InteropServices;
 
 namespace nFMOD
-{     
+{
+    /// <summary>
+    ///  Settings for advanced features like configuring memory and cpu usage for the FMOD_CREATECOMPRESSEDSAMPLE feature.     
+    /// </summary>
+    /// <remarks>
+    /// maxMPEGcodecs / maxADPCMcodecs / maxXMAcodecs will determine the maximum cpu usage of playing realtime samples.  Use this to lower potential excess cpu usage and also control memory usage.
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct AdvancedSettings
+    {
+        /// <summary>
+        /// Size of structure. Use sizeof(AdvancedSettings).
+        /// </summary>
+        public int cbsize;
+
+        /// <summary>
+        /// For use with FMOD_CREATECOMPRESSEDSAMPLE only.
+        /// MPEG codecs consume 48,696 bytes per instance and this number will determine how many MPEG
+        /// channels can be played simultaneously.
+        /// </summary>
+        /// <remarks>
+        /// Default: 16
+        /// </remarks>
+        public int maxMPEGcodecs;
+
+        /// <summary>
+        /// For use with FMOD_CREATECOMPRESSEDSAMPLE only. ADPCM codecs consume 1k per instance and
+        /// this number will determine how many ADPCM channels can be played simultaneously.
+        /// </summary>
+        /// <remarks>
+        /// Default: 32
+        /// </remarks>
+        public int maxADPCMcodecs;
+
+        /// <summary>
+        /// For use with FMOD_CREATECOMPRESSEDSAMPLE only.
+        /// XMA codecs consume 8k per instance and this number will determine how many XMA channels
+        /// can be played simultaneously.
+        /// </summary>
+        /// <remarks>
+        /// Default: 32
+        /// </remarks>
+        public int maxXMAcodecs;
+
+        [Obsolete("PS3 only")]
+        public int maxPCMcodecs;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. For use with FMOD_CREATECOMPRESSEDSAMPLE only. CELT codecs consume
+        /// 11,500 bytes per instance and this number will determine how many CELT channels can be played simultaneously.
+        /// </summary>
+        /// <remarks>
+        /// Default: 16
+        /// </remarks>
+        public int maxCELTcodecs;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. For use with FMOD_CREATECOMPRESSEDSAMPLE only. Vorbis codecs consume
+        /// 12,000 bytes per instance and this number will determine how many Vorbis channels can be played simultaneously.
+        /// </summary>
+        /// <remarks>
+        /// Default: 32
+        /// </remarks>
+        public int maxVORBIScodecs;
+
+        /// <summary>
+        /// [in/out]
+        /// </summary>        
+        public int ASIONumChannels;
+
+        /// <summary>
+        /// [in/out]
+        /// </summary>
+        public IntPtr ASIOChannelList;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. Pointer to a list of speakers that the ASIO channels map to.
+        /// This can be called after System::init to remap ASIO output.
+        /// </summary>
+        public IntPtr ASIOSpeakerList;
+
+        /// <summary>
+        /// [in/out] The max number of 3d reverb DSP's in the system.
+        /// </summary>
+        public int max3DReverbDSPs;
+
+        /// <summary>
+        /// [in/out] For use with FMOD_INIT_HRTF_LOWPASS. The angle (0-360) of a 3D sound from the listener's
+        /// forward vector at which the HRTF function begins to have an effect.
+        /// </summary>
+        /// <remarks>
+        /// Default: 180.0f
+        /// </remarks>
+        public float HRTFMinAngle;
+
+        /// <summary>
+        /// [in/out] For use with FMOD_INIT_HRTF_LOWPASS.  The angle (0-360) of a 3D sound from the listener's
+        /// forward vector at which the HRTF function begins to have maximum effect.  Default = 360.0
+        /// </summary>
+        /// <remarks>
+        /// Default: 360.0f
+        /// </remarks>
+        public float HRTFMaxAngle;
+
+        /// <summary>
+        /// [in/out] For use with FMOD_INIT_HRTF_LOWPASS. The cutoff frequency of the HRTF's lowpass filter function
+        /// when at maximum effect (i.e. at HRTFMaxAngle).
+        /// </summary>
+        /// <remarks>
+        /// Default: 4000.0f
+        /// </remarks>
+        public float HRTFFreq;
+
+        /// <summary>
+        ///  [in/out] For use with FMOD_INIT_VOL0_BECOMES_VIRTUAL. If this flag is used, and the volume is 0.0, then the sound
+        /// will become virtual. Use this value to raise the threshold to a different point where a sound goes virtual.
+        /// </summary>
+        public float vol0virtualvol;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. For use with FMOD Event system only.  Specifies the number of slots
+        /// available for simultaneous non blocking loads.
+        /// </summary>
+        /// <remarks>
+        /// Default: 32
+        /// </remarks>
+        public int eventqueuesize;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. For streams. This determines the default size of the double buffer (in
+        /// milliseconds) that a stream uses.
+        /// </summary>
+        /// <remarks>
+        /// Default: 400
+        /// </remarks>
+        public uint defaultDecodeBufferSize;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. Gives fmod's logging system a path/filename. Normally the log is placed
+        /// in the same directory as the executable and called fmod.log. When using System::getAdvancedSettings, provide at
+        /// least 256 bytes of memory to copy into.
+        /// </summary>
+        public string debugLogFilename;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. For use with FMOD_INIT_ENABLE_PROFILE. Specify the port to listen on
+        /// for connections by the profiler application.
+        /// </summary>
+        public ushort profileport;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. The maximum time in miliseconds it takes for a channel to fade to the
+        /// new level when its occlusion changes.
+        /// </summary>
+        public uint geometryMaxFadeTime;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. The maximum number of buffers for use with getWaveData/getSpectrum.
+        /// </summary>
+        public uint maxSpectrumWaveDataBuffers;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. The delay the music system should allow for loading a sample from
+        /// disk (in milliseconds).
+        /// </summary>
+        /// <remarks>
+        /// Default: 400
+        /// </remarks>
+        public uint musicSystemCacheDelay;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. For use with FMOD_INIT_DISTANCE_FILTERING. The default center
+        /// frequency in Hz for the distance filtering effect.
+        /// </summary>
+        /// <remarks>
+        /// Default: 1500.0f
+        /// </remarks>
+        public float distanceFilterCenterFreq;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. Specify the stack size for the FMOD Stream thread in bytes.
+        /// Useful for custom codecs that use excess stack.
+        /// </summary>
+        /// <remarks>
+        /// Default: 49152 (48kb)
+        /// </remarks>
+        public uint stackSizeStream;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. Specify the stack size for the FMOD_NONBLOCKING loading thread.
+        /// Useful for custom codecs that use excess stack.
+        /// </summary>
+        /// <remarks>
+        /// Default: 65,536 (64kb)
+        /// </remarks>
+        public uint stackSizeNonBlocking;
+
+        /// <summary>
+        /// [in/out] Optional. Specify 0 to ignore. Specify the stack size for the FMOD mixer thread. Useful for
+        /// custom dsps that use excess stack. 
+        /// </summary>
+        /// <remarks>
+        /// Default: 49152 (48kb)
+        /// </remarks>
+        public uint stackSizeMixer;
+    }
+
     /// <summary>
     /// These are plugin types defined for use with the System::getNumPlugins / System_GetNumPlugins, 
     /// System::getPluginInfo / System_GetPluginInfo and System::unloadPlugin / System_UnloadPlugin functions.
