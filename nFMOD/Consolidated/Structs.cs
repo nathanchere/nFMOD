4eaec8ca71411fd135e4f2147b9c94f670f6d3f6
Nathan Chere <git@nathanchere.com.au>
2014-04-07 18:14:57 +1000

    Soundsystem structs

--- nFMOD/Consolidated/Structs.cs
+++ nFMOD/Consolidated/Structs.cs
@@ -16,7 +16,22 @@ namespace nFMOD
 		public int MaximumFrequency { get; internal set; }
 		public SpeakerMode SpeakerMode { get; internal set; }
 		
-		public override string ToString ()
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+
+    public struct RecordDriverDTO
+	{
+		internal int Id { get; set; }
+		public string Name { get; internal set; }
+		public Guid Guid { get; internal set; }
+		public Capabilities Capabilities { get; internal set; }
+		public int MinimumFrequency { get; internal set; }
+		public int MaximumFrequency { get; internal set; }
+		
+		public override string ToString()
 		{
 			return Name;
 		}
