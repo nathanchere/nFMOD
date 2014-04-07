7e32e727fe3d393a9f080d4e564fabeb5ece4e12
Nathan Chere <git@nathanchere.com.au>
2014-04-07 18:15:49 +1000

    Consolidate soundsystem

--- nFMOD/SoundSystem.cs
+++ nFMOD/SoundSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
@@ -866,10 +867,68 @@ namespace nFMOD
             }
         }
         #endregion
-        
+
+        #region Record driver
+        public IEnumerable<RecordDriverDTO> RecordDrivers {
+			get {
+				int Numb = this.NumberRecordDrivers;
+				for (int i = 0; i < Numb; i++) {
+					yield return this.GetRecordDriver(i);
+				}
+			}
+		}
+		
+		private int NumberRecordDrivers {
+			get {
+				int numdrivers;
+				
+				ErrorCode ReturnCode = GetRecordNumDrivers (this.DangerousGetHandle (), out numdrivers);
+				Errors.ThrowIfError (ReturnCode);
+				
+				return numdrivers;
+			}
+		}
+		
+		private void GetRecordDriverInfo(int Id, out string Name, out Guid DriverGuid)
+		{
+			System.Text.StringBuilder str = new System.Text.StringBuilder(255);
+			
+			ErrorCode ReturnCode = GetRecordDriverInfo (this.DangerousGetHandle (), Id, str, str.Capacity, out DriverGuid);
+			Errors.ThrowIfError (ReturnCode);
+			
+			Name = str.ToString();
+		}
+		
+		private void GetRecordDriverCapabilities (int id, out Capabilities caps, out int minfrequency, out int maxfrequency)
+		{
+			ErrorCode ReturnCode = GetRecordDriverCaps (this.DangerousGetHandle (), id, out caps, out minfrequency, out maxfrequency);
+			Errors.ThrowIfError (ReturnCode);
+		}
+		
+		private RecordDriverDTO GetRecordDriver (int Id)
+		{
+			Guid DriverGuid;
+			string DriverName;
+			this.GetRecordDriverInfo(Id, out DriverName, out DriverGuid);
+			
+			Capabilities caps;
+			int minfrequency, maxfrequency;
+			this.GetRecordDriverCapabilities(Id, out caps, out minfrequency, out maxfrequency);
+			
+			return new RecordDriverDTO {
+				Id = Id,
+				Name = DriverName,
+				Guid = DriverGuid,
+				
+				Capabilities = caps,
+				MinimumFrequency = minfrequency,
+				MaximumFrequency = maxfrequency,
+			};
+		}	
+        #endregion
+
         #region Output driver
-        
-		public OutputDriverDTO OutputDriver {
+        public OutputDriverDTO OutputDriver {
 			get {
 				int driver;
 				ErrorCode ReturnCode = GetDriver (this.DangerousGetHandle (), out driver);
