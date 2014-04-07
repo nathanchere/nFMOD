d1c5355a4d79ad289c6d175bdb5ef78b96ceffcd
Nathan Chere <git@nathanchere.com.au>
2014-04-07 16:06:13 +1000

    Cleanup

--- nFMOD/Memory/Memory.cs
+++ nFMOD/Memory/Memory.cs
@@ -4,27 +4,20 @@ using System.Security;
 
 namespace nFMOD.Memory
 {
-	public class Memory
-	{
-		public Memory ()
-		{
-		}
+    public class Memory
+    {
+        [DllImport(Common.FMOD_DLL, EntryPoint = "FMOD_Memory_GetStats"), SuppressUnmanagedCodeSecurity]
+        private static extern ErrorCode GetStats_External(ref int currentalloced, ref int maxalloced, bool blocking);
 
-		public static void GetStats (ref int currentalloced, ref int maxalloced)
-		{
-			ErrorCode ReturnCode = GetStats_External (ref currentalloced, ref maxalloced, true);
-			Errors.ThrowIfError (ReturnCode);
-		}
+        public static void GetStats(ref int currentalloced, ref int maxalloced)
+        {
+            Errors.ThrowIfError(GetStats_External(ref currentalloced, ref maxalloced, true));
+        }
 
-		public static void GetStats (ref int currentalloced, ref int maxalloced, bool blocking)
-		{
-			ErrorCode ReturnCode = GetStats_External (ref currentalloced, ref maxalloced, blocking);
-			Errors.ThrowIfError (ReturnCode);
-		}
-
-		[DllImport(Common.FMOD_DLL, EntryPoint = "FMOD_Memory_GetStats"), SuppressUnmanagedCodeSecurity]
-		private static extern ErrorCode GetStats_External (ref int currentalloced, ref int maxalloced, bool blocking);
-		
-	}
+        public static void GetStats(ref int currentalloced, ref int maxalloced, bool blocking)
+        {
+            Errors.ThrowIfError(GetStats_External(ref currentalloced, ref maxalloced, blocking));
+        }
+    }
 }
 
