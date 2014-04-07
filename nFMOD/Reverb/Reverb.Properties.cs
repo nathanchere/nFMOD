a691d645a9a89519aa3346c72addd2b87c2439ca
Nathan Chere <git@nathanchere.com.au>
2014-04-07 16:28:23 +1000

    Cleanup

--- nFMOD/Reverb/Reverb.Properties.cs
+++ nFMOD/Reverb/Reverb.Properties.cs
@@ -4,51 +4,10 @@ using System.Runtime.InteropServices;
 namespace nFMOD
 {
     public partial class Reverb
-    {
-        //TODO end subbmary
-
-        /*
-    [STRUCTURE] 
-    [
-        [DESCRIPTION]
-        Structure defining a reverb environment.<br>
-        <br>
-        For more indepth descriptions of the reverb properties under win32, please see the EAX2 and EAX3
-        documentation at http://developer.creative.com/ under the 'downloads' section.<br>
-        If they do not have the EAX3 documentation, then most information can be attained from
-        the EAX2 documentation, as EAX3 only adds some more parameters and functionality on top of 
-        EAX2.
-
-        [REMARKS]
-        Note the default reverb properties are the same as the FMOD_PRESET_GENERIC preset.<br>
-        Note that integer values that typically range from -10,000 to 1000 are represented in 
-        decibels, and are of a logarithmic scale, not linear, wheras float values are always linear.<br>
-        <br>
-        The numerical values listed below are the maximum, minimum and default values for each variable respectively.<br>
-        <br>
-        <b>SUPPORTED</b> next to each parameter means the platform the parameter can be set on.  Some platforms support all parameters and some don't.<br>
-        EAX   means hardware reverb on FMOD_OUTPUTTYPE_DSOUND on windows only (must use FMOD_HARDWARE), on soundcards that support EAX 1 to 4.<br>
-        EAX4  means hardware reverb on FMOD_OUTPUTTYPE_DSOUND on windows only (must use FMOD_HARDWARE), on soundcards that support EAX 4.<br>
-        I3DL2 means hardware reverb on FMOD_OUTPUTTYPE_DSOUND on windows only (must use FMOD_HARDWARE), on soundcards that support I3DL2 non EAX native reverb.<br>
-        GC    means Nintendo Gamecube hardware reverb (must use FMOD_HARDWARE).<br>
-        WII   means Nintendo Wii hardware reverb (must use FMOD_HARDWARE).<br>
-        PS2   means Playstation 2 hardware reverb (must use FMOD_HARDWARE).<br>
-        SFX   means FMOD SFX software reverb.  This works on any platform that uses FMOD_SOFTWARE for loading sounds.<br>
-        <br>
-        Members marked with [in] mean the user sets the value before passing it to the function.<br>
-        Members marked with [out] mean FMOD sets the value to be used after the function exits.<br>
-
-        [PLATFORMS]
-        Win32, Win64, Linux, Linux64, Macintosh, Xbox360, PlayStation 2, PlayStation Portable, PlayStation 3, Wii
-
-        [SEE_ALSO]
-        System::setReverbProperties
-        System::getReverbProperties
-        FMOD_REVERB_PRESETS
-        FMOD_REVERB_FLAGS
-    ]
-    */
-
+    {                
+        /// <summary>
+        /// Structure defining a reverb environment.
+        /// </summary>
         [StructLayout(LayoutKind.Sequential)]
         public struct Properties
         {
@@ -89,7 +48,7 @@ namespace nFMOD
             /// <summary>
             /// EAX4 only. Environment Instance.
             /// 3 seperate reverbs simultaneously are possible.
-            /// This specifies which one to set. (win32 only) 
+            /// This specifies which one to set.
             /// </summary>
             public int Instance
             {
@@ -102,7 +61,7 @@ namespace nFMOD
             }
 
             /// <summary>
-            /// sets all listener properties (win32/ps2)
+            /// Sets all listener properties
             /// </summary>
             public int Environment
             {
