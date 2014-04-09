1b138929a7c16df6979139110f4ee17353839115
Nathan Chere <git@nathanchere.com.au>
2014-04-09 17:35:51 +1000

    Cleanup Handle class

--- nFMOD/Handle.cs
+++ nFMOD/Handle.cs
@@ -3,24 +3,22 @@ using System.Runtime.InteropServices;
 
 namespace nFMOD
 {
-	public abstract class Handle : SafeHandle
-	{
-		public Handle() : this(IntPtr.Zero)
-		{
-		}
-		public Handle (IntPtr Handle) : this(Handle, true)
-		{
-		}
-		public Handle (IntPtr Handle, bool OwnsHandle) : base(IntPtr.Zero, OwnsHandle)
-		{
-			SetHandle(Handle);
-		}
-		
-		public override bool IsInvalid {
-			get {
-				return (handle == IntPtr.Zero || (int)handle == -1);
-			}
-		}
-		
-	}
+    public abstract class Handle : SafeHandle
+    {
+        public Handle() : this(IntPtr.Zero) { }
+        public Handle(IntPtr Handle) : this(Handle, true) { }
+        public Handle(IntPtr Handle, bool OwnsHandle) : base(IntPtr.Zero, OwnsHandle)
+        {
+            SetHandle(Handle);
+        }
+
+        public override bool IsInvalid
+        {
+            get
+            {
+                return (handle == IntPtr.Zero || (int)handle == -1);
+            }
+        }
+
+    }
 }
