42bf092563525feb3eeb9536fc0218b59d9dfe57
Nathan Chere <git@nathanchere.com.au>
2014-04-10 12:57:54 +1000

    SA form

--- nFMOD.Demo.SpectrumAnalysis/Program.cs
+++ nFMOD.Demo.SpectrumAnalysis/Program.cs
@@ -16,7 +16,7 @@ namespace nFMOD.Demo.SpectrumAnalysis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
---
+++ nFMOD.Demo.SpectrumAnalysis/frmMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nFMOD.Demo.SpectrumAnalysis
+{
+    public partial class frmMain : Form
+    {
+        private const string MP3_PATH = @"test.mp3";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            var fmod = new FmodSystem();
+            fmod.Init();
+            var sound = fmod.CreateSound(MP3_PATH);
+            fmod.PlaySound(sound);
+        }
+    }
+}
