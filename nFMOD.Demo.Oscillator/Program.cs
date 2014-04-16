0413df85dc9784c009462a1c892b44514bfd2cc4
Nathan Chere <git@nathanchere.com.au>
2014-04-16 13:47:15 +1000

    Cleanup oscillator demo

--- nFMOD.Demo.Oscillator/Program.cs
+++ nFMOD.Demo.Oscillator/Program.cs
@@ -10,16 +10,18 @@ namespace nFMOD.Demo
 {
     class Program
     {
+        Oscillator oscillator;
+
         static void Main(string[] args)
         {
-
             var quit = new ManualResetEvent(false);
             Console.CancelKeyPress += (s, a) => {
                 quit.Set();
                 a.Cancel = true;
             };
 
-            using (var fmod = new FmodSystem()) {
+            using (var fmod = new FmodSystem())
+            {
                 fmod.Init();                
                 using (var oscillator = (Oscillator)fmod.CreateDsp(DspType.Oscillator))
                 {
@@ -27,41 +29,47 @@ namespace nFMOD.Demo
 
                     while (!quit.WaitOne(0))
                     {
-                        Console.SetCursorPosition(0,0);
-                        var prompt = string.Format(
-                            "nFMOD test: DSP (Oscillator)\n" + 
-                            "----------------------------\n\n" + 
-                            "Generating {0} wave; frequency: {1:0}hz\n" + 
-                            "Left/Right to change frequency, Ctrl+C to quit",
-                            oscillator.WaveformType, oscillator.Frequency);
-                        Console.WriteLine(prompt);
-
-                        switch (Console.ReadKey().Key)
-                        {
-                            case ConsoleKey.Escape:
-                                quit.Set();
-                                break;
-
-                            case ConsoleKey.LeftArrow:
-                                oscillator.Frequency -= 10f;
-                                break;
-
-                            case ConsoleKey.RightArrow:
-                                oscillator.Frequency += 10f;
-                                break;
-
-                            case ConsoleKey.Spacebar:
-                                oscillator.CycleWaveforms();
-                                break;
-                        }
-
+                        ShowPrompt(oscillator);
+                        ProcessInput(quit, oscillator);
                         Thread.Sleep(1);
                     }
-
-                    quit.WaitOne();
                 }
                 fmod.CloseSystem();
             }
+        }
+
+        private static void ProcessInput(ManualResetEvent quit, Oscillator oscillator)
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.Escape:
+                    quit.Set();
+                    break;
+
+                case ConsoleKey.LeftArrow:
+                    oscillator.Frequency -= 10f;
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    oscillator.Frequency += 10f;
+                    break;
+
+                case ConsoleKey.Spacebar:
+                    oscillator.CycleWaveforms();
+                    break;
+            }
+        }
+
+        private static void ShowPrompt(Oscillator oscillator)
+        {
+            Console.SetCursorPosition(0, 0);
+            var prompt = string.Format(
+                "nFMOD test: DSP (Oscillator)\n" +
+                "----------------------------\n\n" +
+                "Generating {0} wave; frequency: {1:0}hz\n" +
+                "Left/Right to change frequency, Ctrl+C to quit",
+                oscillator.WaveformType, oscillator.Frequency);
+            Console.WriteLine(prompt);
         }
     }
 }
