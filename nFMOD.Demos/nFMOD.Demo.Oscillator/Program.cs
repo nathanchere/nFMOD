43c041b67c7b9926f59bffdd4545af5f20fbbe75
Nathan Chere <git@nathanchere.com.au>
2014-04-16 14:04:23 +1000

    Add Oscillator.Gui project

---
+++ nFMOD.Demos/nFMOD.Demo.Oscillator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using nFMOD.Dsps;
+using System.Threading;
+
+namespace nFMOD.Demo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            (new Program()).Run();
+        }
+
+        Oscillator oscillator;
+
+        void Run()
+        {
+            var quit = new ManualResetEvent(false);
+            Console.CancelKeyPress += (s, a) => {
+                quit.Set();
+                a.Cancel = true;
+            };
+
+            using (var fmod = new FmodSystem())
+            {
+                fmod.Init();                
+                using (oscillator = (Oscillator)fmod.CreateDsp(DspType.Oscillator))
+                {
+                    oscillator.Play();
+
+                    while (!quit.WaitOne(0))
+                    {
+                        ShowPrompt();
+                        ProcessInput(quit);
+                        Thread.Sleep(1);
+                    }
+                }
+                fmod.CloseSystem();
+            }
+        }
+
+        void ProcessInput(ManualResetEvent quit)
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.Escape:
+                    quit.Set();
+                    break;
+
+                case ConsoleKey.LeftArrow:
+                    oscillator.Frequency -= 10f;
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    oscillator.Frequency += 10f;
+                    break;
+
+                    case ConsoleKey.UpArrow:
+                    oscillator.Frequency += 100f;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    oscillator.Frequency -= 100f;
+                    break;
+
+                case ConsoleKey.Spacebar:
+                    oscillator.CycleWaveforms();
+                    break;
+            }
+        }
+
+        private void ShowPrompt()
+        {
+            Console.SetCursorPosition(0, 0);
+            var prompt = string.Format(
+                "nFMOD test: DSP (Oscillator)\n" +
+                "----------------------------\n\n" +
+                "Generating {0} wave; frequency: {1:0}hz                 \n" +
+                "Left/Right to change frequency in 10hz increments,\n"+
+                "Up/Down to change frequency in 100hz increments,\n"+
+                "Ctrl+C to quit",
+                oscillator.WaveformType, oscillator.Frequency);
+            Console.WriteLine(prompt);
+        }
+    }
+}
