a77dc65c4e87362c17c2b9d1badebc1b266fd0d1
Nathan Chere <git@nathanchere.com.au>
2014-04-17 14:17:50 +1000

    BAsics of DrumKit demo

---
+++ nFMOD.Demos/nFMOD.Demo.DrumKit/frmMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace nFMOD.Demo
+{
+    public partial class frmMain : Form
+    {        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
