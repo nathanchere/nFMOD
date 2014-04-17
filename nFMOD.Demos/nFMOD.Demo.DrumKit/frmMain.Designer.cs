617ced273fdbbb3b7053eaae39386640b1638f80
Nathan Chere <git@nathanchere.com.au>
2014-04-17 16:06:36 +1000

    Trying to improve drumkit performance

--- nFMOD.Demos/nFMOD.Demo.DrumKit/frmMain.Designer.cs
+++ nFMOD.Demos/nFMOD.Demo.DrumKit/frmMain.Designer.cs
@@ -16,110 +16,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnHihatOpen = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnSnare = new System.Windows.Forms.Button();
-            this.btnHihatClosed = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnHihatOpen
-            // 
-            this.btnHihatOpen.Location = new System.Drawing.Point(14, 68);
-            this.btnHihatOpen.Name = "btnHihatOpen";
-            this.btnHihatOpen.Size = new System.Drawing.Size(71, 35);
-            this.btnHihatOpen.TabIndex = 0;
-            this.btnHihatOpen.Text = "HH (Open)";
-            this.btnHihatOpen.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.btnSnare);
-            this.panel1.Controls.Add(this.btnHihatClosed);
-            this.panel1.Controls.Add(this.btnHihatOpen);
-            this.panel1.Location = new System.Drawing.Point(30, 239);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(426, 118);
-            this.panel1.TabIndex = 1;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(259, 37);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(71, 35);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Tom (Low)";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(166, 27);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(71, 35);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Tom (Mid)";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(213, 80);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 35);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Kick";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // btnSnare
-            // 
-            this.btnSnare.Location = new System.Drawing.Point(103, 68);
-            this.btnSnare.Name = "btnSnare";
-            this.btnSnare.Size = new System.Drawing.Size(71, 35);
-            this.btnSnare.TabIndex = 2;
-            this.btnSnare.Text = "Snare";
-            this.btnSnare.UseVisualStyleBackColor = true;
-            this.btnSnare.Click += new System.EventHandler(this.btnSnare_Click);
-            // 
-            // btnHihatClosed
-            // 
-            this.btnHihatClosed.Location = new System.Drawing.Point(14, 37);
-            this.btnHihatClosed.Name = "btnHihatClosed";
-            this.btnHihatClosed.Size = new System.Drawing.Size(71, 35);
-            this.btnHihatClosed.TabIndex = 1;
-            this.btnHihatClosed.Text = "HH (Closed)";
-            this.btnHihatClosed.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 369);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(430, 234);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
             this.Name = "frmMain";
             this.Text = "nFMOD Demo: Drum Kit";
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyUp);
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnHihatOpen;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnSnare;
-        private System.Windows.Forms.Button btnHihatClosed;        
     }
 }
 
--- nFMOD.Demos/nFMOD.Demo.DrumKit/frmMain.cs
+++ nFMOD.Demos/nFMOD.Demo.DrumKit/frmMain.cs
@@ -9,26 +9,27 @@ namespace nFMOD.Demo
     {
         FmodSystem _fmod;
         Dictionary<DrumType, Sound> _drums;
-
+        Dictionary<DrumType, Channel> _channels;
 
         #region ctor etc
         public frmMain()
         {
             InitializeComponent();
             _fmod = new FmodSystem();
-            _fmod.Init();
+            _fmod.Init(32, InitFlags.SoftwareHRTF);
 
             _drums = new Dictionary<DrumType, Sound>();
-            _drums[DrumType.Snare] = _fmod.CreateSound("data/snare.wav");
-            _drums[DrumType.TomMid] = _fmod.CreateSound("data/tom_mid.wav");
-            _drums[DrumType.TomLow] = _fmod.CreateSound("data/tom_low.wav");
-            _drums[DrumType.TomFloor] = _fmod.CreateSound("data/tom_floor.wav");
-            _drums[DrumType.Kick] = _fmod.CreateSound("data/kick.wav");
-            _drums[DrumType.HihatOpen] = _fmod.CreateSound("data/cym_hatopen.wav");
-            _drums[DrumType.HihatMid] = _fmod.CreateSound("data/cym_hatmid.wav");
-            _drums[DrumType.HithatClosed] = _fmod.CreateSound("data/cym_hatclosed.wav");
-            _drums[DrumType.CymbalCrash] = _fmod.CreateSound("data/cym_crash.wav");
-            _drums[DrumType.CymbalRide] = _fmod.CreateSound("data/cym_ride.wav");
+            const SoundMode flags = SoundMode.NonBlocking | SoundMode.SoftwareProcessing;
+            _drums[DrumType.Snare] = _fmod.CreateSound("data/snare.wav", flags);
+            _drums[DrumType.TomMid] = _fmod.CreateSound("data/tom_mid.wav", flags);
+            _drums[DrumType.TomLow] = _fmod.CreateSound("data/tom_low.wav", flags);
+            _drums[DrumType.TomFloor] = _fmod.CreateSound("data/tom_floor.wav", flags);
+            _drums[DrumType.Kick] = _fmod.CreateSound("data/kick.wav", flags);
+            _drums[DrumType.HihatOpen] = _fmod.CreateSound("data/cym_hatopen.wav", flags);
+            _drums[DrumType.HihatMid] = _fmod.CreateSound("data/cym_hatmid.wav", flags);
+            _drums[DrumType.HithatClosed] = _fmod.CreateSound("data/cym_hatclosed.wav", flags);
+            _drums[DrumType.CymbalCrash] = _fmod.CreateSound("data/cym_crash.wav", flags);
+            _drums[DrumType.CymbalRide] = _fmod.CreateSound("data/cym_ride.wav", flags);
         }
 
         protected override void Dispose(bool disposing)
@@ -46,11 +47,7 @@ namespace nFMOD.Demo
         private void PlayDrum(DrumType drumType)
         {
             _fmod.PlaySound(_drums[drumType]);
-        }
-
-        private void btnSnare_Click(object sender, EventArgs e)
-        {
-            _fmod.PlaySound(_drums[DrumType.Snare]);
+            _fmod.Update();
         }
 
         private void frmMain_KeyUp(object sender, KeyEventArgs e)
@@ -60,6 +57,7 @@ namespace nFMOD.Demo
             if (e.KeyCode == Keys.B) PlayDrum(DrumType.TomMid);
             if (e.KeyCode == Keys.N) PlayDrum(DrumType.TomLow);
             if (e.KeyCode == Keys.M) PlayDrum(DrumType.CymbalCrash);
+            if (e.KeyCode == Keys.Space) PlayDrum(DrumType.Kick);
         }
     }
 }
